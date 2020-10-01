0cd6825abe212b65ee51cf04b978006f950a58eb
baa <34665120+baa-mb@users.noreply.github.com>
2020-10-01 14:33:02 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
 function schalte () {
-    schritt = (wink_1 - wink_0) / 10
+    for (let Index = 0; Index <= 10; Index++) {
+        w = schritte * Index * faktor
+        pins.servoWritePin(AnalogPin.P15, startwinkel + w)
+        basic.pause(500)
+    }
     if (lauf_b) {
         faktor = 1
         startwinkel = wink_0
     } else {
         faktor = -1
         startwinkel = wink_1
-    }
-    for (let Index = 0; Index <= 10; Index++) {
-        w = schritt * Index
-        pins.servoWritePin(AnalogPin.P3, wink_0 + w)
-        basic.pause(500)
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -28,13 +27,16 @@ function init () {
     lauf_b = false
     wink_0 = 75
     wink_1 = 152
+    startwinkel = (wink_0 + wink_1) / 2
+    pins.servoWritePin(AnalogPin.P15, startwinkel)
+    schritte = (wink_1 - wink_0) / 10
 }
 function test () {
     for (let index = 0; index < 4; index++) {
-        basic.pause(1000)
-        pins.servoWritePin(AnalogPin.P3, wink_1)
-        basic.pause(1000)
-        pins.servoWritePin(AnalogPin.P3, wink_0)
+        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P15, wink_1)
+        basic.pause(500)
+        pins.servoWritePin(AnalogPin.P15, wink_0)
     }
 }
 input.onButtonPressed(Button.B, function () {
@@ -60,12 +62,11 @@ radio.onReceivedStringDeprecated(function (receivedString) {
     }
 })
 let lauf_a = false
-let w = 0
+let wink_1 = 0
+let wink_0 = 0
+let lauf_b = false
 let startwinkel = 0
 let faktor = 0
-let lauf_b = false
-let wink_0 = 0
-let wink_1 = 0
-let schritt = 0
+let schritte = 0
+let w = 0
 init()
-test()
